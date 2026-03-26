3912bae9b6b0ecba18e68a8ea46aafb58e597a5c
MrSeager <seager1993@gmail.com>
2026-03-26 12:09:16 +0200

    3/26/2026
    Navbar added

---
+++ components/HeaderNav.tsx
@@ -0,0 +1,75 @@
+'use client';
+//Components
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function HeaderNav() {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    return(
+        <header className="max-w-[120rem] w-full">
+            <nav className="relative w-full py-5 px-3 lg:px-10 flex items-center gap-10">
+                <Image 
+                    src={'/images/logo.png'}
+                    alt="logo"
+                    width={162}
+                    height={27}
+                />
+                <div className={`absolute bg-[#fcfcfc] duration-500 w-full right-0 px-3 gap-3 overflow-hidden flex flex-col top-15 
+                                ${isOpen ? 'py-3 max-h-screen' : 'py-0 max-h-0'} 
+                                lg:gap-10 lg:px-10 lg:bg-transparent lg:max-h-screen lg:top-0 lg:flex-row lg:relative`}>
+                    <Link 
+                        href={'/'}
+                        className="font-semibold duration-500
+                                    hover:text-[#43687a]"
+                    >
+                        What is Coding
+                    </Link>
+                    <Link 
+                        href={'/'}
+                        className="font-semibold duration-500
+                                    hover:text-[#43687a]"
+                    >
+                        Curriculum
+                    </Link>
+                    <Link 
+                        href={'/'}
+                        className="font-semibold duration-500
+                                    hover:text-[#43687a]"
+                    >
+                        Glossary
+                    </Link>
+                    <Link 
+                        href={'/'}
+                        className="font-semibold duration-500
+                                    hover:text-[#43687a]"
+                    >
+                        About Us
+                    </Link>
+                    <Link 
+                        href={'/'}
+                        className="font-semibold duration-500
+                                    hover:text-[#43687a]"
+                    >
+                        FAQ
+                    </Link>
+                    <Link 
+                        href={'/'}
+                        className="font-semibold duration-500 lg:ms-auto
+                                hover:text-[#43687a]"
+                    >
+                        Sign In
+                    </Link>
+                </div>
+                <button 
+                    type="button" 
+                    className="ms-auto lg:hidden"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    Menu
+                </button>
+            </nav>
+        </header>
+    );
+}
